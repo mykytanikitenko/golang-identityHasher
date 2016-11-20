1fee9d6f5bdd20a9421f2b96a568f47a300dd7ed
Mykyta Nikitenko <nikitanikitenko@gmail.com>
2016-11-20 17:22:51 +0200

    Created benchmark

---
+++ identityHasher_benchmark_test.go
@@ -0,0 +1,30 @@
+package identityHasher
+
+import "testing"
+
+var benchmarkGenerateAndValidateHashResult bool
+
+func BenchmarkGenerateAndValidateHash(benchmark *testing.B) {
+	for benchmarkIteration := 0; benchmarkIteration < benchmark.N; benchmarkIteration++ {
+		password := getRandomString()
+
+		hashed, err := Hash(password)
+		if err != nil {
+			panic(err)
+		}
+
+		benchmarkGenerateAndValidateHashResult, err = ValidateHash(password, hashed)
+		if err != nil {
+			panic(err)
+		}
+	}
+}
+
+func getRandomString() string {
+	bytes, err := getRandomBytes(32)
+	if err != nil {
+		panic(err)
+	}
+
+	return string(bytes[:])
+}
